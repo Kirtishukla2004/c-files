8b56bb71b3ef826e18a0f8bd67266604f21bf99e
kirti shukla <104082719+shukla2004@users.noreply.github.com>
2022-07-14 02:42:20 +0530

    starting of learning c++  a hello world programme in c++

---
+++ helloworld.cpp
@@ -0,0 +1,11 @@
+// Online C++ compiler to run C++ program online
+#include <stdio.h>
+using namespace std;
+
+int main()
+{
+    // Write C++ code here
+    printf("<< Hello world!");
+
+    return 0;
+}
