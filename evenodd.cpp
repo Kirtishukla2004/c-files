0c9f17c1fd1a47c7e0813e54c0f4fe4298cd3918
kirti shukla <104082719+shukla2004@users.noreply.github.com>
2022-07-14 02:41:34 +0530

    checking weather a number is odd or even by using if else conditional statements in c++ code

---
+++ evenodd.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cout << "enter the number ";
+    cin >> a;
+    if (a % 2 == 0)
+    {
+        cout << " its a even number   " << a;
+    }
+    else
+    {
+        cout << "its  a odd number " << a;
+    }
+}
