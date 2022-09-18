3da5681ce22a4ce3fb5318ecc02ef47e3f071a8c
kirti shukla <104082719+shukla2004@users.noreply.github.com>
2022-09-18 17:55:55 +0530

    Add files via upload

---
+++ add.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int a ,b,c,d,add1,add2,add3,add4,add5;
+	printf("enter a,b= ");
+	scanf("%d %d",&a,&b);
+	add1=a+b;
+	printf("added a*b=:%d",add1);
+	printf("enter c,d=");
+	scanf("%d %d",&c,&d);
+	add2=c+d;
+	printf("added c*d=:%d",add2);
+	
+	printf("enter a,c=");
+	scanf("%d %d",&a,&c);
+	if(add1>add2 )
+	printf("add1");
+	else 
+	{
+	printf("wrong input");
+}
+	getch();
+	
+	
+	
+	getch();
+	
+	
+}
