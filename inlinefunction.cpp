ec9f38e1baf0fd317c2541e200ee50404fed71da
kirti shukla <104082719+shukla2004@users.noreply.github.com>
2022-10-17 01:25:55 +0530

    inline function in cpp

---
+++ inlinefunction.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+inline int even(int a  )
+{
+    return (a % 2==0)?true:false;
+    }
+int main(){
+    int a;
+    cout<<"true= 1 false = 0"<<endl<<even(3);
+   
+
+
+}
